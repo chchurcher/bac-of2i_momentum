fe55cc96fbaa850de34746ea4cabd0b85c688101
Kircher, Christoph <christoph.kircher.56@gmail.com>
2024-05-27 17:48:29 +0200

    brownian motion bool in partical

--- src/classes/Particle.m
+++ src/classes/Particle.m
@@ -6,6 +6,7 @@ classdef Particle
         rotation;
         unitVecMat;
         diffusionTensor;
+        isBrownianMotion;
     end
     
     methods
@@ -15,23 +16,31 @@ classdef Particle
             obj.position = zeros(3, 1);
             obj.rotation = zeros(3, 1);
             obj.unitVecMat = eye(3);
+            obj.isBrownianMotion = true;
+        end
+
+        function obj = setBrownianMotion(obj, isBrownianMotion)
+            obj.isBrownianMotion = isBrownianMotion;
         end
 
         function posRot = getPosRot(obj)
             posRot = [obj.position; obj.rotation];
         end
 
-        function obj = step(obj, forceTorqueLab, delta_t)
-            largeUnitMat = [inv(obj.unitVecMat), zeros(3);
-                           zeros(3), inv(obj.unitVecMat)];
-            forceTorqueParticle = largeUnitMat * forceTorqueLab;
+        function obj = step(obj, forcTorqLab, delta_t)
 
-            mu = zeros(6, 1);
-            w = mvnrnd(mu, obj.diffusionTensor);
+            forcTorqPart = zeros(6, 1);
+            forcTorqPart(1:3) = obj.unitVecMat \ forcTorqLab(1:3);
+            forcTorqPart(4:6) = obj.unitVecMat \ forcTorqLab(4:6);
 
             deltaPosRot = 1 / (Constants.k_B * Constants.T) ...
-                * delta_t * obj.diffusionTensor * forceTorqueParticle;% ...
-                % + sqrt(2 * delta_t) * w.';
+                * delta_t * obj.diffusionTensor * forcTorqPart;
+            
+            if obj.isBrownianMotion
+                mu = zeros(6, 1);
+                w = mvnrnd(mu, obj.diffusionTensor);
+                deltaPosRot = deltaPosRot + sqrt(2 * delta_t) * w.';
+            end
 
             obj.position = obj.position + obj.unitVecMat * deltaPosRot(1:3);
             obj.rotation = obj.rotation + deltaPosRot(4:6);
