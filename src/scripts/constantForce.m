31033b4776b2353990cecebf242f60368f980520
Kircher, Christoph <christoph.kircher.56@gmail.com>
2024-05-27 18:01:34 +0200

    separated brownianMotion and constantForce

--- src/scripts/constantForce.m
+++ src/scripts/constantForce.m
@@ -1,4 +1,4 @@
-% Brownian motion of an ellipsoid without external forces or torques
+% Constant force introduction to an ellipsoid without brownian motion
 
 % Set seed of the random generator for reproducity
 rng(10);
@@ -9,11 +9,14 @@ delta_t = 0.1;
 end_t = 10;
 
 t = 0:delta_t:end_t;
+
 forcTorq = zeros(6, numel(t));
+forcTorq(:,1:25) = repmat(1e-15 * [1; 0; 0; 0; 1e-2; 0], 1, 25);
+forcTorq(:,26:50) = repmat(1e-15 * [1; 0; 0; 0; 0; 0], 1, 25);
 
 D = DiffusionTensor.ellipsoid(halfAxes);
 particle = Particle(D);
-particle = particle.setBrownianMotion(true);
+particle = particle.setBrownianMotion(false);
 trajectory = Trajectory();
 
 for i = 1:numel(t)
