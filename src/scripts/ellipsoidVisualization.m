d42883a1e457c5c9695c4f152e5cc168818aa947
Kircher, Christoph <christoph.kircher.56@gmail.com>
2024-09-30 09:45:37 +0200

    added fimplicit3 view

--- src/scripts/ellipsoidVisualization.m
+++ src/scripts/ellipsoidVisualization.m
@@ -1,11 +1,17 @@
-halfAxes = [1, 2, 3];
-posRots = [[3, 1, 1, 0, pi/6, 0];
-           [0, 0, 0, 0, pi/6, pi/2];
-           [-1, -1, -1, 0, pi/6, pi/2]]';
-[X, Y, Z, U, V, W] = Transformation.getQuiverAxes(posRots, halfAxes);
+halfAxes = [0.3; 0.2; 0.1];
+posRot = [0; 1; 0; pi/6; 0; 0];
+
+% [X, Y, Z, U, V, W] = Transformation.getQuiverAxes(posRots, halfAxes);
+% figure;
+% quiver3(X, Y, Z, U, V, W);
+
+
+f = @(xyz) (xyz(1)/halfAxes(1)).^2 + (xyz(2)/halfAxes(2)).^2 + (xyz(3)/halfAxes(3)).^2 - 1;
+f2 = @(x,y,z) f(Transformation.toParticle(posRot, [x;y;z]));
 
 figure;
-quiver3(X, Y, Z, U, V, W);
+interval = [-5 5 -5 5 -5 5].*1;
+fimplicit3(f2, interval);
 
 title('Quiver3 of Ellipsoid')
 xlabel('X');
